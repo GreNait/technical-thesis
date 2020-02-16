ced349bffa48bc83f4bf58ad96390e8d1eca1e3b
GreNait <flyingsepl@googlemail.com>
2020-02-16 16:59:20 +0100

    model updated with gpu etc.

--- first_model.py
+++ first_model.py
@@ -2,7 +2,17 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras.callbacks import TensorBoard
 import numpy as np
+import time
+
+NAME = f"First-model-test {int(time.time())}"
+
+print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+
+#%%
+tensorboard = TensorBoard(log_dir=f'logs/{NAME}')
+
 
 # %%
 X = np.load('/home/grenait/Desktop/technical_thesis/technical-thesis/dataCollection/training_data/training_data_X.npy')
@@ -14,16 +24,17 @@ X = X/255
 
 # %%
 model = Sequential()
-model.add(Conv2D(64, (3,3), input_shape = X.shape[1:]))
+model.add(Conv2D(256, (3,3), input_shape = X.shape[1:]))
 model.add(Activation("relu"))
 model.add(MaxPooling2D(pool_size=(2,2)))
 
-model.add(Conv2D(64, (3,3)))
+model.add(Conv2D(256, (3,3)))
 model.add(Activation("relu"))
 model.add(MaxPooling2D(pool_size=(2,2)))
 
 model.add(Flatten())
-model.add(Dense(64))
+model.add(Dense(256))
+model.add(Activation("relu"))
 
 model.add(Dense(1))
 model.add(Activation("sigmoid"))
@@ -31,6 +42,7 @@ model.add(Activation("sigmoid"))
 model.compile(loss="binary_crossentropy", optimizer="adam", metrics=['accuracy'])
 
 # %%
-model.fit(X,y, batch_size = 25, epochs=3, validation_split=0.1)
+model.fit(X,y, batch_size = 25, epochs=100, validation_split=0.1, callbacks=[tensorboard])
+
 
 # %%
